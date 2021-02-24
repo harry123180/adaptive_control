184e2ddb9c6d59d502577b27f9d46708e1bbb3f8
harry123180 <41154762+harry123180@users.noreply.github.com>
2021-02-25 00:41:32 +0800

    新增LMS

--- 自適應.py
+++ 自適應.py
@@ -1,38 +1,39 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+
 simple_point= 100
 x = np.linspace(0, 10, simple_point)
-y = np.cos(x)*30
+
+y = np.cos(x)*3
 y3 = np.zeros(shape=(simple_point))
 M=simple_point# 參數量
 a = np.zeros(shape=(M))
+history_input = np.zeros(shape=(100))
 #print(a)
 估計誤差= 0
 估計輸入 =0
+avg_err=0
 plt.ion()
-cf = 0.00001# 收斂係數 未知的 要問 此湛設為0.01
+cf = 0.001# 收斂係數 未知的 要問 此湛設為0.01
 figure, ax = plt.subplots(figsize=(8, 6))
 line1, = ax.plot(x, y)#CH1 輸入
 line2,= ax.plot(x,y) #CH2 跟CH2
 line3,= ax.plot(x,y) #FIR filter output wave
 
-plt.title("Dynamic Plot of sinx", fontsize=25)
+plt.title("Adaptive control", fontsize=25)
 
 plt.xlabel("X", fontsize=18)
-plt.ylabel("sinX", fontsize=18)
+plt.ylabel("OUTPUT", fontsize=18)
 t = 0
 #y3[0]=1 #測試用
 次數=0
+err_list=[]
 while(True):
     t+=1
     updated_y = np.cos(x - 0.05 * t)*3
-    次數+=1
-    if 次數==10:
-        次數=0
-        for u in range(simple_point):
-            for u_ in range(M):
-                y3[u] += a[u_]*updated_y[u-u_]
+
+
     ######################
     #更新CH1 wave
     line1.set_xdata(x)
@@ -44,32 +45,43 @@ while(True):
     line2.set_ydata(y2)
     ######################################
     #更新line3
-
+    y4 = a.dot(y2)
+    y3[0] = y4
     #y4 大小2 (y4(1),y4(2)) 放進去y3
     # 因為y4只有2個大小 所以要把它放進去有100個點的y3 先把y3的100個點往後移兩位
     y3 = y3.tolist()
-    for i in range(2):  ###右移
+    for i in range(1):  ###右移
         y3.insert(0, y3.pop())
     y3 = np.array(y3)
-    誤差=y3-y2
-    輸入= y2
+    error=y3[0]-y2[0] #純量誤差
+    input_x= y2 # 輸入是向量
+    #y2 type is array of numpy
+    #print(np.shape(y2))
+    err_list.append(error)
 
-    for i in range(len(誤差)):
-        估計誤差 +=誤差[i]*1/len(誤差) #E(d[n])
-    for i in range(len(輸入)):
-        估計輸入+=輸入[i]*1/len(輸入)
-    估計誤差 = round(估計誤差, 2)
-    估計輸入= round(估計輸入,2)
-    估計誤差陣列=[]
+
+    估計誤差 = round(error, 2)#取小數點後二位
+    #估計輸入= round(input_x,2)
+    次數+=1
+
+    if(次數%10==0):
+        total_err = 0
+        for i in range(len(err_list)):
+            total_err = total_err+err_list[i]
+        avg_err = total_err/len(err_list)
+    if(次數>10):
+        for i in range(1):  ###右移
+            err_list.insert(0, err_list.pop())
+    #history_input = np.append(history_input,input_x,axis=0)
+    #print(history_input)
     #print("估計誤差=",估計誤差,"估計輸入",估計輸入)
     for i in range(M):
-        if(i ==M-1):
-            i = i-1
-        a[i+1]=a[i]-2*cf*估計誤差*估計輸入
-    print(a)
+        #a[i] = 1
+        a[i]=a[i]-2*cf*avg_err*input_x[i]
+    print(a[0])
     line3.set_xdata(x)
     line3.set_ydata(y3)
     figure.canvas.draw()
 
     figure.canvas.flush_events()
-    time.sleep(0.1)
+    time.sleep(0.01)
