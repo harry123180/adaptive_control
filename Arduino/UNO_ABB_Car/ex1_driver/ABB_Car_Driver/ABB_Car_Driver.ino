86c16ccefe03a69fd6bc687dfc0caf093c98f376
harry123180 <harry123180@gmail.com>
2021-05-03 18:32:52 +0800

    updata python

--- Arduino/UNO_ABB_Car/ex1_driver/ABB_Car_Driver/ABB_Car_Driver.ino
+++ Arduino/UNO_ABB_Car/ex1_driver/ABB_Car_Driver/ABB_Car_Driver.ino
@@ -6,8 +6,8 @@ left_wheel.attach(10);
 righ_wheel.attach(11);
 }
 void loop() {
-left_wheel.writeMicroseconds(1340);//前進
-righ_wheel.writeMicroseconds(1740);
+//left_wheel.writeMicroseconds(1340);//前進
+//righ_wheel.writeMicroseconds(1740);
 delay(1000);
 left_wheel.writeMicroseconds(1540);//停車
 righ_wheel.writeMicroseconds(1528);
---
+++ Python/濟蓋.py
@@ -0,0 +1,17 @@
+
+
+i = 0
+def sum( m , n ,s):
+    i=m
+    total = 0
+    while(i <= n ):
+        total += i
+        i = i*s
+
+        print(total)
+
+
+#sum(2,1024,2)
+sum(3,729,3)
+#sum(2,2048,4)
+
