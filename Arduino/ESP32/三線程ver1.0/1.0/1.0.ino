2260d19a32558948aba3087b7240a57d1e5c2353
harry123180 <harry123180@gmail.com>
2021-07-15 12:25:23 +0800

    update
    notebook 7/15

--- Arduino/ESP32/三線程ver1.0/1.0/1.0.ino
+++ Arduino/ESP32/三線程ver1.0/1.0/1.0.ino
@@ -1,22 +1,29 @@
 #include "WiFi.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 //https://blog.csdn.net/qq_41868901/article/details/104677389
-
+//********初期宣告************
 const int potPin = 33;
 int val=0;
-const char* ssid = "兔田株式会社";
+const char* ssid = "兔田株式会社";//iPhone
 const char* password =  "pekopeko";
 const uint16_t port = 8090;
-const char * host = "10.1.1.10";
+const char * host = "10.1.1.10";//172.20.10.3
+const uint16_t samping_rate = 1000;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//********初期宣告************
+int i =0;
+const uint32_t wait_time = 1000000/samping_rate;
 void taskOne( void * parameter ){
+  //task1拿來做數據採樣
+  //還要存入陣列
   while(1){
-    //Serial.print("使用核心編號：");
-//Serial.println(xPortGetCoreID());
-   delay(20);
+   delay(wait_time);
    val = analogRead(potPin);
-  //Serial.print("val  = ");
-  Serial.println(val);
   }
-  Serial.println("Ending task 1");
   vTaskDelete( NULL );
 }
  
@@ -34,31 +41,56 @@ void taskTwo( void * parameter){
     }
     client.print(val);
   }
-  Serial.println("Ending task 2");
+  //Serial.println("Ending task 2");
   vTaskDelete( NULL );
 }
  
 void taskThree( void * parameter){
-  while(1){
-    delay(800);
-    Serial.println("3");
+  //oled線程
+  while(1){ 
+    i++;
+  if(i>10000)i=0;
+  Serial.println("working");
+  display.clearDisplay();
+  
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  
+  display.setCursor(0, 10);
+  display.println("RMS="+String(i));
+  display.display();
+  delay(100);
   }
-  Serial.println("Ending task 3");
+  
   vTaskDelete( NULL );
 }
  
  
 void setup() {
-  Serial.begin(115200);
+ Serial.begin(115200);
   //pinMode(potPin,INPUT);
+  //**********OLED 設定 *************
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  /*
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  // Display static text
+  display.println("Hello, world!");
+  display.display(); 
+  */
+  //**********OLED 設定 *************
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.println("...");
   }
  
-  Serial.print("WiFi connected with IP: ");
-  Serial.println(WiFi.localIP());
+  //Serial.print("WiFi connected with IP: ");
+  //Serial.println(WiFi.localIP());
   delay(1000);
   xTaskCreatePinnedToCore(
   taskOne, //本任務實際對應的Function
@@ -68,14 +100,7 @@ void setup() {
   0, //優先序：0為最低，數字越高代表越優先
   NULL, //對應的任務handle變數
   1); //指定執行核心編號（0、1或tskNO_AFFINITY：系統指定）
-  /*
-  xTaskCreate(
-              taskOne,          ///*任务函数
-              "TaskOne",       带任务名称的字符串
-              10000,            堆栈大小，单位为字节
-              NULL,             作为任务输入传递的参数
-              1,                任务的优先级
-              NULL);            任务句柄*/
+
   xTaskCreate(
               taskTwo,          /* Task function. */
               "TaskTwo",        /* String with name of task. */
@@ -83,15 +108,16 @@ void setup() {
               NULL,             /* Parameter passed as input of the task */
               1,                /* Priority of the task. */
               NULL);            /* Task handle. */
-  xTaskCreate(
+  xTaskCreatePinnedToCore(
               taskThree,          /* Task function. */
               "taskThree",        /* String with name of task. */
               10000,            /* Stack size in bytes. */
               NULL,             /* Parameter passed as input of the task */
               1,                /* Priority of the task. */
-              NULL);            /* Task handle. */
+              NULL,
+              0);            /* Task handle. */
 }
  
 void loop(){
-  delay(1000);
+
 }
