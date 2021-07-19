d652ef423c5ca2d3cd4f4da7ca186167b6bf963c
harry123180 <harry123180@gmail.com>
2021-07-19 19:58:59 +0800

    1

--- Python/NCKU/keras_autoencoder.py
+++ Python/NCKU/keras_autoencoder.py
@@ -1,64 +1,59 @@
 import keras
 from keras import layers
-from keras import regularizers
-# This is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+import matplotlib.pyplot as plt
+input_img = keras.Input(shape=(28, 28, 1))
 
-# This is our input image
-input_img = keras.Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = layers.Dense(encoding_dim, activation='relu')(input_img)
-#encoded = layers.Dense(encoding_dim, activation='relu',
-                #activity_regularizer=regularizers.l1(10e-5))(input_img)
-# "decoded" is the lossy reconstruction of the input
-#decoded = layers.Dense(784, activation='sigmoid')(encoded)
-decoded = layers.Dense(784, activation='sigmoid')(encoded)
+x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
 
-# This model maps an input to its reconstruction
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(16, (3, 3), activation='relu')(x)
+x = layers.UpSampling2D((2, 2))(x)
+decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
 autoencoder = keras.Model(input_img, decoded)
-
-# This model maps an input to its encoded representation
-encoder = keras.Model(input_img, encoded)
-# This is our encoded (32-dimensional) input
-encoded_input = keras.Input(shape=(encoding_dim,))
-# Retrieve the last layer of the autoencoder model
-decoder_layer = autoencoder.layers[-1]
-# Create the decoder model
-decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 from keras.datasets import mnist
 import numpy as np
+
 (x_train, _), (x_test, _) = mnist.load_data()
+
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape)
-print(x_test.shape)
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+from keras.callbacks import TensorBoard
+
 autoencoder.fit(x_train, x_train,
                 epochs=50,
-                batch_size=256,
+                batch_size=128,
                 shuffle=True,
-                validation_data=(x_test, x_test))
-# Encode and decode some digits
-# Note that we take them from the *test* set
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
-# Use Matplotlib (don't ask)
-import matplotlib.pyplot as plt
+                validation_data=(x_test, x_test),
+                callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+decoded_imgs = autoencoder.predict(x_test)
 
-n = 10  # How many digits we will display
+n = 10
 plt.figure(figsize=(20, 4))
-for i in range(n):
+for i in range(1, n + 1):
     # Display original
-    ax = plt.subplot(2, n, i + 1)
+    ax = plt.subplot(2, n, i)
     plt.imshow(x_test[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # Display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
+    ax = plt.subplot(2, n, i + n)
     plt.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
